f879c29cecac8cb2e9301e4db07682579cb6c29f
Emmanuel Elias <emmanuelelias455@gmail.com>
2023-03-17 15:19:13 +0100

    handled cors

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { mainConfig } from 'main.config';
 import { AppModule } from './app.module';
-import { EntityNotFoundExceptionFilter } from './common/filters/entity-not-found-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   mainConfig(app);
   const configService = app.get(ConfigService);
-  app.useGlobalFilters(new EntityNotFoundExceptionFilter());
+  app.enableCors();
   await app.listen(configService.get('PORT'), () =>
     console.log(`App started** on port: ${configService.get('PORT')}`),
   );
